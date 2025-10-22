bd30d54c62401046d957154bbbfe16e392dc8d6d
camylla14 <vcamylla222@gmail.com>
2025-10-22 19:08:41 -0300

    instala o biome

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-import app from "./app.js"
+import app from './app.js';
 
-app.listen(3001, () => console.log("Aplication is running at port 3001"));
+app.listen(3001, () => console.log('Aplication is running at port 3001'));
