1e78908e32b0544937501ce9008370dd7b8dccee
camylla14 <vcamylla222@gmail.com>
2025-11-28 17:05:54 -0300

    Upload de imagens com Multer

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController.js';
 import SessionController from './app/controllers/SessionController.js';
 import ProductController from './app/controllers/ProductController.js';
+import multer from 'multer';
+import multerConfig from './config/multer.cjs';
 
 const routes = new Router();
 
+const uploads = multer(multerConfig);
+
 routes.post('/users', UserController.store);
-routes.post('/session', SessionController.store)
-routes.post('/products', ProductController.store)
+routes.post('/session', SessionController.store);
+routes.post('/products', uploads.single('file'), ProductController.store);
+
 export default routes;
