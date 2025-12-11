54a16e90106969031008690a5062e7cb1a20a47c
camylla14 <vcamylla222@gmail.com>
2025-12-10 22:32:18 -0300

    Salva produto no banco

--- src/routes.js
+++ src/routes.js
@@ -12,5 +12,6 @@ const uploads = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 routes.post('/products', uploads.single('file'), ProductController.store);
+routes.get('/products', ProductController.index)
 
 export default routes;
